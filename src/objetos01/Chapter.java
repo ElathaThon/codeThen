25ec0a054a8d6e3fd505b69728849e5fb3cbdea2
Elatha <bernat.90@gmail.com>
2017-07-26 17:58:34 +0200

    Started clases of Chapter and Book

---
+++ src/objetos01/Chapter.java
@@ -0,0 +1,20 @@
+package objetos01;
+
+/**
+ * 3- Cread las clases Book y Chapter. Book tendrá author (o si queréis complicarlo, authors), title y chapters (una List de chapters).
+ * Chapter tendrá title y page. Book tendrá un método que devolverá el número total de páginas del libro,
+ * y en su método toString retornará los datos del libro junto a los títulos de capítulos junto con su número de páginas,
+ *
+ * por ejemplo así:
+ * Head First Java
+ * by Kathy Sierra
+ * 1 - Breaking the Surface: page 1
+ * 2 - A Trip to Objectville: page 27
+ * 3 - Know Your Variables: page 49
+ * etc.
+ * */
+
+public class Chapter {
+
+
+}
