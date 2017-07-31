16fa28a944ba98a31bf852579e80371e1b1f9953
Elatha <bernat.90@gmail.com>
2017-07-31 19:28:50 +0200

    added samples de classe del ferran

---
+++ src/examples/examplesCodeThen/samples/classes/CarV1.java
@@ -0,0 +1,8 @@
+package examples.examplesCodeThen.samples.classes;
+
+/** Simple class, no constructor or methods, just public properties */
+public class CarV1 {
+
+	public String brand;
+	public int speed;
+}
