7c2026aefb01a7c8f2cabea291afb9e891034a84
Elatha <bernat.90@gmail.com>
2017-08-01 16:51:18 +0200

    delated samples

--- src/examples/examplesCodeThen/samples/classes/CarV1.java
+++
@@ -1,8 +0,0 @@
-package examples.examplesCodeThen.samples.classes;
-
-/** Simple class, no constructor or methods, just public properties */
-public class CarV1 {
-
-	public String brand;
-	public int speed;
-}
